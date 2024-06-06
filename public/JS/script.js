e9f6177a71bd7553de5382e49ad7dd9104d35dc7
Sarah Darko <saradarko22@gmail.com>
2024-06-06 11:44:12 +0000

    Math in Javascript

--- public/JS/script.js
+++ public/JS/script.js
@@ -69,6 +69,46 @@ const bottle1={
 
 
 
+  // Functions
+
+// Defining Functions
+
+function login(username,password){
+  // Validate username and password
+  if(!username|| !password){
+    return'Username or password not provided';
+  }
+  // Verify username and password
+  if(username=='Sarah'&& password =='1234'){
+    return'User is logged in';
+  }
+  else{
+    return 'Invalid username or password';
+  }
+}
+
+// Invoking a Function
+login('Sarah' ,'1234');
+
+//  Basic Arithmetic Operations
+11+12;
+1+0.5;
+43-12;
+18/6
+45/5
+45/7;
+5*3;
+60%5
+45-24
+Math.round(18/5);
+Math.random()*1000;
+Math.max(34,56,45);
+2*(3+4)-5/2
+'2'+'2'
+Number('2')+2
+Number('a')+2
+
+
 
 
 
